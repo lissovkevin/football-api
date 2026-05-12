121a957486c4cfd1e0e0d076c079cf625a4e66ca
Kevin Lissov <kl223py@student.lnu.se>
2026-05-12 21:39:34 +0900

    Add league resolver

---
+++ src/graphql/resolvers/leagueResolvers.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "../../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+export const leagueResolvers = {
+  leagues: async () => {
+    return prisma.league.findMany({ orderBy: { name: "asc" } });
+  },
+
+  league: async ({ id }) => {
+    const league = await prisma.league.findUnique({
+      where: { id },
+      include: {
+        matches: {
+          include: { homeTeam: true, awayTeam: true, league: true },
+          take: 10,
+        },
+      },
+    });
+    if (!league) throw new Error(`NOT_FOUND: League with id ${id} does not exist.`);
+    return league;
+  },
+};
